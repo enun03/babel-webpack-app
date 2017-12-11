8e3054d7884e1fc2b0b85a0e7810cdbdde6751a6
Eli Nunez <enunez025@gmail.com>
2017-12-11 20:49:55 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const people = require('./public/js/people.js');
+
+people.forEach(function(person){
+    console.log(person);
+})
---
+++ public/js/people.js
@@ -0,0 +1,6 @@
+const people = [
+        {name: 'Mari', age: 4},
+        {name: 'Jane' , age: 20}
+    ]
+
+module.exports = people;
